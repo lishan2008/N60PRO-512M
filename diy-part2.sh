86221e1a99ca1def90bf746d8de162320250d4b6
lishan2008 <sunrain1974@gmail.com>
2026-03-27 08:27:56 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -4,7 +4,8 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 # 
-#
+# 512rom
+ sed -i 's/7a80000/1ea00000/g' target/linux/mediatek/dts/mt7986a-netcore-n60-pro.dts
 
 # Modify default IP
  sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
