5d856d57ebf51924d98edbf6580d4fc18c342a97
lishan2008 <sunrain1974@gmail.com>
2026-04-28 17:31:58 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,8 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+# Add openappfilter source
+  git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFiltere
+  chmod -R 755 ./package/OpenAppFiltere/*
+
